2e299ee42229a394655532c2aed0f4a39a2c9230
Siwakorn longsomboon <siwakorn.l@icit.kmutnb.ac.th>
2023-06-11 02:22:43 +0700

    sync all teacher

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -63,7 +63,7 @@ class TeacherController extends Controller
             ->leftJoin('faculty','faculty.id','=','teacher.faculty_id')
             ->leftJoin('department','department.id','=','teacher.department_id');
         }
-        
+
         if($request->includeFaculty){
             $items->addSelect('faculty.name_th as faculty_name');
             $items->leftJoin('faculty','faculty.id','=','teacher.faculty_id');
@@ -149,7 +149,7 @@ class TeacherController extends Controller
         }else{
             $items = $items->orderBy('teacher.id', 'asc');
         }
-    
+
         $count = $items->count();
         $perPage = $request->perPage ? $request->perPage : $count;
         $currentPage = $request->currentPage ? $request->currentPage : 1;
@@ -158,7 +158,7 @@ class TeacherController extends Controller
         $offset = $perPage * ($currentPage - 1);
         $items = $items->skip($offset)->take($perPage);
         $items = $items->get();
-    
+
         return response()->json([
             'message' => 'success',
             'data' => $items,
@@ -212,49 +212,60 @@ class TeacherController extends Controller
         ], 200);
     }
 
-    // public function add(Request $request)
-    // {
-    //     $request->validate([
-    //         'name as required',
-    //         'name as required',
-    //         'email as required',
-    //         'account_type as required',
-    //     ]);
-        
-    //     // $data = $request->all();
-    //     // $data['column9'] = 'aaaa';
-    //     // Province::create($data);
+    public function add(Request $request)
+    {
+        $request->validate([
+            'citizen_id as required',
+            'person_key as required',
+        ]);
 
-    //     // Province::create([
-    //     //     'provincename' => $request->provincename,
-    //     //     'name' => $request->name,
-    //     //     'email' => $request->email,
-    //     //     'citizen_id' => $request->citizen_id,
-    //     //     'account_type' => $request->account_type,
-    //     // ]);
+        $item = new Teacher;
 
-    //     $data = new Province;
-    //     $data->provincename = $request->provincename;
-    //     $data->name = $request->name;
-    //     $data->email = $request->email;
-    //     $data->citizen_id = $request->citizen_id;
-    //     $data->account_type = $request->account_type;
-    //     $data->save();
+        $pathSignature = null;
+        if(($request->signature_file != "") && ($request->signature_file != 'null') && ($request->signature_file != 'undefined')){
+            $fileSignature = 'signature-'.rand(10,100).'-'.$request->file('signature_file')->getClientOriginalName();
+            $pathSignature = '/teacher/signature/'.$fileSignature;
+            Storage::disk('public')->put($pathSignature, file_get_contents($request->signature_file));
+            $request->signature_file = $pathSignature;
+        }
 
-    //     $responseData = [
-    //         'message' => 'success',
-    //         'data' => $data,
-    //     ];
+        $item->person_key = $request->person_key;
+        $item->citizen_id = $request->citizen_id;
+        $item->prefix = $request->prefix;
+        $item->citizen_id = $request->citizen_id;
+        $item->firstname = $request->firstname;
+        $item->surname = $request->surname;
+        $item->tel = $request->tel;
+        $item->email = $request->email;
+        $item->signature_file = $pathSignature;
+        $item->address = $request->address;
+        $item->province_id = $request->province_id;
+        $item->amphur_id = $request->amphur_id;
+        $item->tumbol_id = $request->tumbol_id;
+        $item->faculty_id = $request->faculty_id;
+        $item->department_id = $request->department_id;
+        $item->updated_by = 'arnonr';
+        $item->save();
 
-    //     return response()->json($responseData, 200);
-    // }
+        $responseData = [
+            'message' => 'success',
+            'data' => $item,
+        ];
+
+        return response()->json($responseData, 200);
+    }
 
     public function edit($id, Request $request)
     {
-        $request->validate([
-            'id as required',
-        ]);
-
+        // $request->validate([
+        //     'id as required',
+        // ]);
+            // print_r($request);
+        if(!empty($request->citizen_id)){
+            echo "xxx";
+        }else{
+            echo "yyy";
+        }
         $item = Teacher::where('id', $id)->first();
 
         $pathSignature = null;
@@ -267,10 +278,11 @@ class TeacherController extends Controller
             $pathSignature  = $item->signature_file;
         }
 
+        $item->person_key = $request->has('person_key') ? $request->person_key: $item->person_key;
+        $item->citizen_id = $request->has('citizen_id') ? $request->citizen_id: $item->citizen_id;
         $item->prefix = $request->has('prefix') ? $request->prefix: $item->prefix;
         $item->firstname = $request->has('firstname') ? $request->firstname: $item->firstname;
         $item->surname = $request->has('surname') ? $request->surname: $item->surname;
-        // $item-> = $request->has('') ? $request->: $item->;
         $item->tel = $request->has('tel') ? $request->tel : $item->tel;
         $item->email = $request->has('email') ? $request->email : $item->email;
         $item->signature_file = $pathSignature;
@@ -279,8 +291,22 @@ class TeacherController extends Controller
         $item->amphur_id  = $request->has('amphur_id') ? $request->amphur_id : $item->amphur_id;
         $item->tumbol_id = $request->has('tumbol_id') ? $request->tumbol_id : $item->tumbol_id;
         $item->active = $request->has('active') ? $request->active : $item->active;
+        $item->faculty_id = $request->has('faculty_id') ? $request->faculty_id : $item->faculty_id;
+        $item->department_id = $request->has('department_id') ? $request->department_id : $item->department_id;
+        $item->hris_last_updated_at = $request->has('hris_last_updated_at') ? $request->hris_last_updated_at : $item->hris_last_updated_at;
+        // $item->hris_last_updated_at = '2023-06-11 01:54:00';
+        // $item->department_id = 7;
+
+        // echo '$item->hris_last_updated_atxx'.$item->hris_last_updated_at;
+
         $item->updated_by = 'arnonr';
-        $item->save();
+        // print_r($item);
+        // $item->save();
+        if($item->save()){
+            echo "OK";
+        }else{
+            echo "Fail";
+        }
 
         $responseData = [
             'message' => 'success',
@@ -293,7 +319,7 @@ class TeacherController extends Controller
     public function delete($id)
     {
         $data = Teacher::where('id', $id)->first();
-        
+
         $data->deleted_at = Carbon::now();
         $data->save();
 
@@ -309,9 +335,9 @@ class TeacherController extends Controller
         $dataParams = [];
 
         if ($request->person_key) $dataParams["person_key"] = $request->person_key;
-    
+
         if ($request->citizen_id) $dataParams["citizen_id"] = $request->citizen_id;
-    
+
         $dataParams["get_work_info"] = 1;
         $dataParams["get_citizen_id"] = 1;
         // dataParams['get_icit_account'] = 1;
@@ -357,7 +383,6 @@ class TeacherController extends Controller
         }
     }
 
-
     public function getHrisPersonel(Request $request) {
 
         $request->validate([
@@ -366,7 +391,7 @@ class TeacherController extends Controller
         ]);
 
         $access_token = 'rn7496A7JE7jEnstEbAQDsm2bstbKhaW'; // <----- API - Access Token Here
-        // 
+        //
         $data = [
             'faculty_code' => 14,
         ];
@@ -390,7 +415,7 @@ class TeacherController extends Controller
         $api_url = "https://api.hris.kmutnb.ac.th/api/personnel-api/list-personnel"; // <----- API URL
 
         $response = Http::timeout(50)->withToken($access_token)->post($api_url, $data);
-     
+
         if($response != false){
             if (!empty($response['data'])) {
                 $api_data = [];
@@ -421,17 +446,173 @@ class TeacherController extends Controller
         }
     }
 
+    public function apiListHrisPersonnel($params)
+    {
+        $access_token = 'rn7496A7JE7jEnstEbAQDsm2bstbKhaW'; // <----- API - Access Token Here
+        //
+        $data = [
+            'faculty_code' => 14,
+        ];
 
-    public function hrisSyncAllTeacher(Request $req) {
+        if (!empty($params['firstname'])){
+            $data['firstname'] = $params['firstname'];
+        }
+
+        if (!empty($params['surname'])){
+            $data['surname'] = $params['surname'];
+        }
+
+        if (!empty($params['position_type_id'])){
+            $data['position_type_id'] = $params['position_type_id'];
+        }
+
+        if (!empty($params['person_key'])){
+            $data['person_key'] = $params['person_key'];
+        }
+
+        $api_url = "https://api.hris.kmutnb.ac.th/api/personnel-api/list-personnel"; // <----- API URL
+
+        $response = Http::timeout(50)->withToken($access_token)->post($api_url, $data);
+
+        $api_data = [];
+
+        if($response != false){
+            if (!empty($response['data'])) {
+
+                foreach($response['data'] as $value){
+                    $api_data[] = [
+                        'person_key' => $value['person_key'],
+                        'last_updated_at' => $value['last_updated_at'],
+                        'firstname' => $value['firstname_th'],
+                        'surname' => $value['lastname_th'],
+                        'position_type_id' => $value['position_type_id'],
+                    ];
+                }
+
+            }
+        }
+        return $api_data;
+    }
+
+    public function apiHrisPersonnelDetail($params) {
+        $dataParams = [];
+
+        if (!empty($params['person_key'])) $dataParams["person_key"] = $params['person_key'];
+
+        if (!empty($params['citizen_id'])) $dataParams["citizen_id"] = $params['citizen_id'];
+
+        $dataParams["get_work_info"] = 1;
+        $dataParams["get_citizen_id"] = 1;
+        // dataParams['get_icit_account'] = 1;
+
+        $access_token = 'rn7496A7JE7jEnstEbAQDsm2bstbKhaW'; // <----- API - Access Token Here
+
+        $api_url = 'https://api.hris.kmutnb.ac.th/api/personnel-api/personnel-detail'; // <----- API URL
+
+        $response = Http::timeout(50)->withToken($access_token)->post($api_url, $dataParams);
+        $apiData = [];
+        if($response != false){
+            $apiData = [
+                'person_key' => $response['person_key'],
+                'citizen_id' => $response['person_info']['citizen_id'],
+                'person_photo' => $response['person_photo'],
+                'last_updated_at' => $response['last_updated_at'],
+                'prefix' => $response['person_info']['full_prefix_name_th'],
+                'firstname' => $response['person_info']['firstname_th'],
+                'surname' => $response['person_info']['lastname_th'],
+                'faculty_code' => $response['work_info']['faculty_code'],
+                'faculty_name' => $response['work_info']['faculty_name_th'],
+                'department_code' => $response['work_info']['department_code'],
+                'department_name' => $response['work_info']['department_name_th'],
+                'position_id' => $response['work_info']['position_id'],
+                'position_th' => $response['work_info']['position_th'],
+            ];
+        }
+        return $apiData;
+    }
+
+    function hrisSyncAllTeacher(Request $req) {
+        /* ดึงข้อมูลสายวิชาการทั้งหมด */
+        $result = $this->apiListHrisPersonnel(['position_type_id' => 1, 'firstname' => 'ทวีศักดิ์']);
+        $insert = 0;
+        $update = 0;
+        foreach ($result as $key => $value) {
+            $person_key = $value['person_key'];
+            $api_updated_time = $value['last_updated_at'];
+            $person_name = $value['firstname'].' '.$value['surname'];
+
+            $teacher_data = Teacher::where('person_key', $person_key)->first();
+            // print_r($teacher_data);
+            echo "api".$api_updated_time;
+            echo '\ndb'.$teacher_data->hris_last_updated_at;
+            if ($teacher_data) {
+                if($teacher_data->hris_last_updated_at == $api_updated_time){
+                    // echo $teacher_data->firstname."Already exists\n";
+                    continue;
+                }
+                // echo $teacher_data->firstname."Update";
+                // echo "\n";
+            }
+
+            $result_detail = $this->apiHrisPersonnelDetail(['person_key' => $person_key]);
+
+            $citizen_id = $result_detail['citizen_id'];
+            $prefix = $result_detail['prefix'];
+            $firstname = $result_detail['firstname'];
+            $surname = $result_detail['surname'];
+            $faculty_code = $result_detail['faculty_code'];
+            $faculty_name = $result_detail['faculty_name'];
+            $department_code = $result_detail['department_code'];
+            $department_name = $result_detail['department_name'];
+            $position_id = $result_detail['position_id'];
+            $position_th = $result_detail['position_th'];
+
+            // print_r($result_detail);
+            $faculty = app('App\Http\Controllers\FacultyController')->import($faculty_code,$faculty_name);
+            $department = app('App\Http\Controllers\DepartmentController')->import($department_code,$department_name);
+
+            $reqTeacher = new Request();
+            $reqTeacher->person_key = $person_key;
+            $reqTeacher->citizen_id = $citizen_id;
+            $reqTeacher->prefix = $prefix;
+            $reqTeacher->firstname = $firstname;
+            $reqTeacher->surname = $surname;
+            $reqTeacher->faculty_id = $faculty->id;
+            $reqTeacher->department_id = $department->id;
+            $reqTeacher->hris_last_updated_at = $api_updated_time;
+
+            // print_r($result_detail);
+
+            if ($teacher_data) {
+                echo "ID ".$teacher_data->id;
+                $save_data = $this->edit($teacher_data->id, $reqTeacher);
+                $update++;
+            }else{
+                $save_data = $this->add($reqTeacher);
+                $insert++;
+                echo "Insert";
+            }
+        }
+
+        $responseData = [
+            'message' => 'success',
+            'data' => ['insert' => $insert, 'update' => $update],
+        ];
+
+        return response()->json($responseData, 200);
+    }
+
+
+    public function OldhrisSyncAllTeacher(Request $req) {
             /* ดึงข้อมูลสายวิชาการทั้งหมด */
 
             $req = new Request();
             $req->position_type_id = 1;
-            // 
+            //
             $result = $this->getHrisPersonel($req);
-        
+
             foreach($result as $key => $value){
-                
+
                 print_r($value);
 
                 $person_key = $result[$key]['person_key'];
@@ -443,7 +624,7 @@ class TeacherController extends Controller
                 $teacher_data = Teacher::where('person_key', $person_key)->first();
 
                 $db_updated_time = null;
-                
+
                 if ($teacher_data) {
                     $db_updated_time = $api_updated_time;
 
@@ -453,13 +634,13 @@ class TeacherController extends Controller
                 }
 
                 $req1 = new Request();
-                $req1->person_key = person_key;
+                $req1->person_key = $person_key;
 
                 $resultInfo = $this->hrisPersonnelInfo($req1);
 
                 $faculty = app('App\Http\Controllers\FacultyController')->import($result->faculty_code,$result->faculty_name);
                 $major = app('App\Http\Controllers\DepartmentController')->import($result->department_code,$result->department_name);
-    
+
                 $result['faculty_id'] = $faculty->id;
                 $result['department_id'] = $department->id;
 
@@ -488,5 +669,5 @@ class TeacherController extends Controller
             }
             // res.success(result);
     }
-    
+
 }
